8a19535a3eccc516d7b23143b569c3bd387396a3
ankitraj805173 <ankitraj805173@gmail.com>
2025-08-29 13:39:46 +0530

    Created using Colab

---
+++ PythonCollab.py
@@ -0,0 +1,95 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNeoLF6P57dE2+X1nE8uzYY",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/ankitraj805173/codsoft/blob/main/PythonCollab.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "WHjsJaJZ2Re1"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "i = 0\n",
+        "while i < 10:\n",
+        "    print(i, end=' ')\n",
+        "    i += 1"
+      ],
+      "metadata": {
+        "id": "1BaJatzy1WCZ",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "c8eda107-8d5a-4138-d8d3-b9fe2376b7cc"
+      },
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "0 1 2 3 4 5 6 7 8 9 "
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [],
+      "metadata": {
+        "id": "0ey-gAGG1VRN"
+      }
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "3s_7eM2D0103",
+        "outputId": "a82b332f-1d74-4d5a-96a2-5698fbda0e88"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hallo World\n"
+          ]
+        }
+      ],
+      "source": [
+        "print(\"hallo World\")\n"
+      ]
+    }
+  ]
+}
